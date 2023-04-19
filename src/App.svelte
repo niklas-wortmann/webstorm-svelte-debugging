0ba4ee754dd945b3135a95699f1e29619e69fcd0
Jan-Niklas Wortmann <jan-niklas.wortmann@evorait.com>
2023-04-19 13:18:19 -0500

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,19 @@
+<script>
+
+    export let counter = 0;
+</script>
+
+<main>
+    <button on:click={() => counter--}>Decrease</button>
+    <span>{counter}</span>
+    <button on:click={() => counter++}>Increase</button>
+</main>
+
+<style>
+    main {
+        text-align: center;
+        padding: 1em;
+        max-width: 240px;
+        margin: 0 auto;
+    }
+</style>
